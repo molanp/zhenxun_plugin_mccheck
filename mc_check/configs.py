5252cb45fe04163931553ba98ce8a12757b177e2
molanp <104612722+molanp@users.noreply.github.com>
2026-01-29 18:38:18 +0800

    Replace ujson with json for loading data

--- mc_check/configs.py
+++ mc_check/configs.py
@@ -1,13 +1,13 @@
 import os
 
-import ujson
+import json
 
 from zhenxun.configs.config import Config
 
 
 def readInfo(file: str) -> dict:
     with open(os.path.join(os.path.dirname(__file__), file), encoding="utf-8") as f:
-        return ujson.loads((f.read()).strip())
+        return json.loads((f.read()).strip())
 
 message_type = Config.get_config("mc_check", "type", 0)
 lang = Config.get_config("mc_check", "LANGUAGE", "zh-cn")
